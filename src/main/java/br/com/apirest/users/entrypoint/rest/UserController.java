6f75b310087749ef5c08295bae8949f9ca292010
= <williamfincatti.dasilva@gamil.com>
2020-06-11 21:02:46 -0300

    change travis file

--- src/main/java/br/com/apirest/users/entrypoint/rest/UserController.java
+++ src/main/java/br/com/apirest/users/entrypoint/rest/UserController.java
@@ -2,6 +2,10 @@ package br.com.apirest.users.entrypoint.rest;
 
 import br.com.apirest.users.domain.entity.User;
 import br.com.apirest.users.usecase.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +15,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+@ApiModel(subTypes = {User.class})
 @RestController
 @CrossOrigin(origins = "http://localhost:8081")
 @RequestMapping("/api/v1/users")
@@ -31,16 +36,23 @@ public class UserController {
         this.editUser = editUser;
     }
 
+    @ApiOperation(value = "Find user by id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "status code 200 user found"),
+            @ApiResponse(code = 404, message = "status code 200 user not found")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<User> findUserById(@PathVariable Integer id) {
         return ResponseEntity.ok(findUser.execute(id));
     }
 
+    @ApiOperation(value = "List all users")
     @GetMapping
     public ResponseEntity<List<User>> listUsers() {
         return ResponseEntity.ok(listUsers.execute());
     }
 
+    @ApiOperation(value = "Create new user")
     @PostMapping
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User userCreated = createUser.execute(user);
@@ -48,12 +60,14 @@ public class UserController {
         return ResponseEntity.created(uri).body(userCreated);
     }
 
+    @ApiOperation(value = "If user exists delete")
     @DeleteMapping("/{id}")
     public ResponseEntity deleteUser(@PathVariable("id") Integer idUser) {
         deleteUser.execute(idUser);
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value = "Update user")
     @PutMapping
     public ResponseEntity editUser(@Valid @RequestBody User user) {
         editUser.execute(user);
