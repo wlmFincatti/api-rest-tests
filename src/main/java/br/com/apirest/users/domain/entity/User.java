038a97fa3cffec2b942ebddd5489a53fdb26d46f
= <williamfincatti.dasilva@gamil.com>
2020-06-12 21:50:51 -0300

    fixed bean validation

--- src/main/java/br/com/apirest/users/domain/entity/User.java
+++ src/main/java/br/com/apirest/users/domain/entity/User.java
@@ -6,10 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
 @Setter
@@ -22,12 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Min(value = 18, message = "Age should not be less than 18")
-    @Max(value = 150, message = "Age should not be greater than 150")
     private Integer age;
-    @NotNull
-    @Size(min = 5, max = 200, message
-            = "Name must be between 5 and 200 characters")
     private String name;
 
 }
--- src/main/java/br/com/apirest/users/entrypoint/dto/UserToEntity.java
+++ src/main/java/br/com/apirest/users/entrypoint/dto/UserToEntity.java
@@ -5,11 +5,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserToEntity {
+    @NotNull(message = "Name not to be null")
+    @Size(min = 5, max = 200, message
+            = "Name must be between 5 and 200 characters")
     private String name;
+    @NotNull(message = "Age Not to be null")
+    @Min(value = 18, message = "Age should not be less than 18")
+    @Max(value = 150, message = "Age should not be greater than 150")
     private Integer age;
 }
